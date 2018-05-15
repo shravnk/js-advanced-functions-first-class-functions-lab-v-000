ce2231322c092882890aecfe6ae2d0aaa6d38934
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-15 21:31:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const returnLastTwoDrivers = function (drivers) {
   return newArray = drivers.slice(-2)
 }
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(mult) {
+  return function(fare){fare.mult}
+}
