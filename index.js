f11db109f86e655cadc42dfab0373cbe6ba4ad75
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-15 21:33:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,5 @@ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 function createFareMultiplier(mult) {
   return function(fare){return fare*mult}
 }
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
